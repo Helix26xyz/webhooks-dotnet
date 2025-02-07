7b997c3d47916bcaf74d1a5a1f15e5911f7b04cb
Ryan Shaut <ryan@shaut.us>
2025-02-07 13:53:12 -0500

    Add basic migration logic

---
+++ webhooks.StorageMigrations/migrations/10_schema.sql
@@ -0,0 +1,92 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[WebhookStatus]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE WebhookStatus (
+        Value int NOT NULL PRIMARY KEY,
+        [Name] varchar(50) NOT NULL
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[WebhookEventStatus]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE WebhookEventStatus (
+        Value int NOT NULL PRIMARY KEY,
+        [Name] varchar(50) NOT NULL
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[WebhookEventSubStatus]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE WebhookEventSubStatus (
+        Value int NOT NULL PRIMARY KEY,
+        [Name] varchar(50) NOT NULL
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Webhooks]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE Webhooks (
+        Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+        Name NVARCHAR(MAX) NOT NULL DEFAULT '',
+        Slug NVARCHAR(MAX) NOT NULL DEFAULT '',
+        Url NVARCHAR(MAX) NOT NULL DEFAULT '',
+        CreatedAt DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
+        CreatedBy NVARCHAR(MAX) NOT NULL DEFAULT '',
+        Owner NVARCHAR(MAX) NOT NULL DEFAULT '',
+        Project NVARCHAR(MAX) NOT NULL DEFAULT '',
+        Status int NOT NULL,
+        FOREIGN KEY (Status) REFERENCES WebhookStatus(Value)
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[WebhookEvents]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE WebhookEvents (
+        Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+        WebhookId UNIQUEIDENTIFIER NOT NULL,
+        CreatedAt DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
+        UpdatedAt DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
+        Payload NVARCHAR(MAX) NULL,
+        Status int NOT NULL,
+        SubStatus int NOT NULL,
+        FOREIGN KEY (WebhookId) REFERENCES Webhooks(Id),
+        FOREIGN KEY (Status) REFERENCES WebhookEventStatus(Value),
+        FOREIGN KEY (SubStatus) REFERENCES WebhookEventSubStatus(Value)
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Migrations]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE Migrations (
+        Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+        MigrationName NVARCHAR(255) NOT NULL,
+        AppliedAt DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
+        Backend NVARCHAR(50) NOT NULL,
+        Description NVARCHAR(MAX) NULL
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM WebhookStatus)
+BEGIN
+    INSERT INTO WebhookStatus (Value, Name) VALUES
+    (1,'enabled'),
+    (2,'disabled'),
+    (3,'suspended')
+END
+
+IF NOT EXISTS (SELECT * FROM WebhookEventStatus)
+BEGIN
+    INSERT INTO WebhookEventStatus (Value, Name) VALUES
+    (1,'new'),
+    (2,'received'),
+    (3,'processed')
+END
+
+IF NOT EXISTS (SELECT * FROM WebhookEventSubStatus)
+BEGIN
+    INSERT INTO WebhookEventSubStatus (Value, Name) VALUES
+    (0,'pending'),
+    (1,'success'),
+    (2,'failed'),
+    (3,'retry'),
+    (4, 'skipped')
+END
--- webhooks.StorageMigrations/src/DatabaseMigrationService.cs
+++ webhooks.StorageMigrations/src/DatabaseMigrationService.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Data;
 
 namespace webhooks.StorageMigrations.src
 {
@@ -15,9 +17,13 @@ namespace webhooks.StorageMigrations.src
 
         public async Task EnsureDatabaseMigratedAsync()
         {
+           Console.WriteLine("Checking if database exists");
             if (!await DatabaseExistsAsync())
             {
+                Console.WriteLine("Creating database");
                 await CreateDatabaseAsync();
+
+
             }
 
             await ApplyMigrationsAsync();
@@ -38,7 +44,14 @@ namespace webhooks.StorageMigrations.src
                 command.CommandText = $"SELECT database_id FROM sys.databases WHERE Name = '{databaseName}'";
 
                 var result = await command.ExecuteScalarAsync();
-                return result != null;
+                if (result == null)
+                {
+                    Console.WriteLine("Database does not exist");
+                } else
+                {
+                    Console.WriteLine("Database exists");
+                }
+                    return result != null;
             }
         }
 
@@ -51,31 +64,104 @@ namespace webhooks.StorageMigrations.src
 
             using (var connection = new SqlConnection(builder.ConnectionString))
             {
-                await connection.OpenAsync();
+                if (connection.State != ConnectionState.Open)
+                {
+                    await connection.OpenAsync();
 
+                }
+
+                Console.WriteLine($"Creating database {databaseName}");
                 var command = connection.CreateCommand();
                 command.CommandText = $"CREATE DATABASE [{databaseName}]";
 
                 await command.ExecuteNonQueryAsync();
+
+            }
+            using (var connection = new SqlConnection(_connectionString))
+            { 
+            // run 10_schema.sql
+            Console.WriteLine("Applying 10_schema.sql");
+            var schemaFilePath = Path.Combine(_migrationsFolder, "10_schema.sql");
+            var schemaCommandText = await File.ReadAllTextAsync(schemaFilePath);
+            await RunSQLNonQueryCommandAsync(schemaCommandText, connection);
+            await MarkMigrationCompleteAsync("10_schema.sql", connection); }
+        }
+        static private async Task RunSQLNonQueryCommandAsync(string commandText, SqlConnection connection )
+        {
+
+                if (connection.State != ConnectionState.Open)
+                {
+                    await connection.OpenAsync();
+
+                }
+                using (var command = new SqlCommand(commandText, connection))
+                {
+                {
+                    int affectedRows = await command.ExecuteNonQueryAsync();
+                    Console.WriteLine($"Command executed successfully. Affected rows: {affectedRows}");
+                }
             }
         }
+        private async Task<SqlDataReader> RunSQLQueryReadAsync(string commandText, SqlConnection connection )
+        {
 
+                if (connection.State != ConnectionState.Open)
+                {
+                    await connection.OpenAsync();
+
+                }
+                using (var command = new SqlCommand(commandText, connection))
+                {
+                    var res = await command.ExecuteReaderAsync();
+                    return res;
+                }
+        }
+        private async Task MarkMigrationCompleteAsync(string migrationName, SqlConnection connection )
+        {
+
+                var commandText = $"INSERT INTO [dbo].[Migrations] ([MigrationName], [Backend]) VALUES ('{migrationName}', 'CoreDatabase')";
+                await RunSQLNonQueryCommandAsync(commandText, connection);
+        }
         private async Task ApplyMigrationsAsync()
         {
             var sqlFiles = Directory.GetFiles(_migrationsFolder, "*.sql");
 
+            // sort by name
+            Array.Sort(sqlFiles);
+
             using (var connection = new SqlConnection(_connectionString))
             {
-                await connection.OpenAsync();
+                // get current migration status
+                var query = "SELECT [MigrationName] FROM [dbo].[Migrations] WHERE [Backend] = 'CoreDatabase'";
+                var appliedMigrations = new List<string>();
+
+                using (var reader = await RunSQLQueryReadAsync(query, connection))
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        appliedMigrations.Add(reader.GetString(0));
+                    }
+                }
+
+                if (connection.State != ConnectionState.Open)
+                {
+                    await connection.OpenAsync();
+                }
 
                 foreach (var file in sqlFiles)
                 {
-                    var commandText = await File.ReadAllTextAsync(file);
+                    var fileName = Path.GetFileName(file);
 
-                    using (var command = new SqlCommand(commandText, connection))
+                    // check if the migration has already been applied
+                    if (appliedMigrations.Contains(fileName))
                     {
-                        await command.ExecuteNonQueryAsync();
+                        Console.WriteLine($"Skipping already applied migration: {fileName}");
+                        continue;
                     }
+
+                    var commandText = await File.ReadAllTextAsync(file);
+                    await RunSQLNonQueryCommandAsync(commandText, connection);
+                    await MarkMigrationCompleteAsync(fileName, connection);
                 }
             }
         }
