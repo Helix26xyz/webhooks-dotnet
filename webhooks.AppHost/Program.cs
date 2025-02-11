b0970e2f0e342f5fd7d368cb80b4e7b9a417906c
Ryan Shaut <ryan@shaut.us>
2025-02-11 17:15:31 -0500

    fix merge conflict

--- webhooks.AppHost/Program.cs
+++ webhooks.AppHost/Program.cs
@@ -20,22 +20,10 @@ var apiService = builder.AddProject<Projects.webhooks_ApiService>("apiservice")
 
 var cache = builder.AddRedis("cache");
 
-var sql = builder.AddSqlServer("sql")
-                 .WithLifetime(ContainerLifetime.Persistent);
-
-var db = sql.AddDatabase("Data-312adef9-a1d1-447f-9a88-c4836ac84dcb");
-
 builder.AddProject<Projects.webhooks_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
     .WaitFor(cache)
-<<<<<<< HEAD
-=======
-    .WithReference(db)
-    .WaitFor(db)
-    .WithReference(storageMigrationsClient)
-    .WaitFor(storageMigrationsClient)
->>>>>>> 22d6086b7c907916990c96824d703913f6557f90
     .WithReference(apiService)
     .WaitFor(apiService);
 
